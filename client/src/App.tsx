9b8b34aa26fe9e6ff5644c84996de52d787361b9
spriveen <rpeanuka@gmail.com>
2026-06-04 18:02:51 +0530

    updat app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
@@ -10,8 +10,10 @@ import History from "./pages/History";
 import RankTracker from "./pages/RankTracker";
 import RankDetail from "./pages/RankDetail";
 import { Toaster } from "react-hot-toast";
+import { useApp } from "./context/AppContext";
 
 export default function App() {
+     const {user, loading} = useApp();
     const location = useLocation();
 
     const hideNavbar = ["/login", "/register"].includes(location.pathname);
@@ -22,8 +24,13 @@ export default function App() {
             {!hideNavbar && <Navbar />}
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login state="login" />} />
-                <Route path="/register" element={<Login state="register" />} />
+
+                <Route path="/login" element={ user ? <Navigate to='/dashboard' replace /> : 
+                <Login state="login" />} />
+
+                <Route path="/register" element={ user ? <Navigate to='/dashboard' replace />  :
+                   <Login state="register" />} />
+                   
                 <Route element={<ProtectedRoute />}>
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/analyze" element={<Analyze />} />
